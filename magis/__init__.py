68c8d0b52d5c9863804e0a20e7e3a2aaea2bf99c
Brian <brian.c.mcmahan@gmail.com>
2017-02-27 21:23:41 -0500

    woops

--- magis/__init__.py
+++ magis/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import os
 TOP_PATH = curdir = os.path.dirname(os.path.abspath(__file__))
 
-import utils
+#import utils
 from .utils import stopwatch
 from . import models
 from . import data
